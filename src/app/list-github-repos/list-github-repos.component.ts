7cc5fb4adcadef3f204cb9cafbf3fb943282c96a
Zoechbauer <hans.zoechbauer@aon.at>
2020-04-13 13:28:43 +0200

    Some code enhancements

--- src/app/list-github-repos/list-github-repos.component.ts
+++ src/app/list-github-repos/list-github-repos.component.ts
@@ -31,24 +31,42 @@ export class ListGithubReposComponent implements OnInit, OnDestroy {
   constructor(private githubService: GithubService) {}
 
   ngOnInit(): void {
-    // this.formSubscription = this.filterForm.valueChanges.subscribe((values) => {
-    //   console.log(this.filterProp);
-    //   console.log('FormValues', this.filterForm.value);
-    // });
-
     this.filterProperties = this.githubService.getFilterProperties();
 
     // github api is called in a loop until all records of the organization are received
+    // whenever a next Page Number oberservable arrives, the api is called
     this.githubService.pageNumberSubject.subscribe((nextPageNumber) => {
       this.pageNumber = nextPageNumber;
       console.log('Loop api: pageNumber', this.pageNumber);
-      this.filterReposApi();
+
+      this.subscription = this.githubService
+        .getGitHubOrgRepos(this.searchOrg, this.pageNumber)
+        .subscribe(
+          (repos: GitHubOrgRepo[]) => {
+            // add repos to array
+            this.repos.push(...repos);
+            this.errors = false;
+            // increment pagenumber or exit api
+            this.pageNumber = repos.length > 0 ? this.pageNumber++ : 0;
+            if (this.pageNumber === 0) {
+              this.searchText = this.searchTextOld;
+            }
+            console.log(
+              'api-repos this.repos',
+              repos.length,
+              this.repos.length
+            );
+          },
+          (errors) => {
+            this.errors = true;
+            console.log('ERR in filterRepos', errors);
+          }
+        );
     });
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
-    // this.formSubscription.unsubscribe();
   }
 
   filterRepos() {
@@ -56,28 +74,6 @@ export class ListGithubReposComponent implements OnInit, OnDestroy {
     this.searchText = 'please wait ...';
     this.repos = [];
     this.pageNumber = 1;
-    this.filterReposApi();
-  }
-
-  filterReposApi() {
-    this.subscription = this.githubService
-      .getGitHubOrgRepos(this.searchOrg, this.pageNumber)
-      .subscribe(
-        (repos: GitHubOrgRepo[]) => {
-          // add repos to array
-          this.repos.push(...repos);
-          this.errors = false;
-          // increment pagenumber or exit api
-          this.pageNumber = repos.length > 0 ? this.pageNumber++ : 0;
-          if (this.pageNumber === 0) {
-            this.searchText = this.searchTextOld;
-          }
-          console.log('api-repos this.repos', repos.length, this.repos.length);
-        },
-        (errors) => {
-          this.errors = true;
-          console.log('ERR in filterRepos', errors);
-        }
-      );
+    this.githubService.pageNumberSubject.next(this.pageNumber);
   }
 }
